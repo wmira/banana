db3f9ccc1f36d4425a2e98aeca8fc252719c5838
Warren Mira <warrenmira@gmail.com>
2022-09-02 22:11:56 +0800

    jest fail

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,13 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { nanoid } from 'nanoid'
+import * as React from 'react'
 
 const Home: NextPage = () => {
+  React.useEffect(() => {
+    console.log(nanoid())
+  }, [])
   return (
     <div className={styles.container}>
       <Head>
---
+++ test/index.test.tsx
@@ -0,0 +1,6 @@
+import { render} from '@testing-library/react'
+import App from '../pages/index'
+
+describe('it does not work', () => {
+  render(<App />)
+})
